df36696666c4f5887c41597d005df6b740230edf
Ashish Yadav <ashish.ya@HCL.COM>
2019-10-14 18:59:09 +0530

    commit for project files

---
+++ src/main/java/com/ashish/ashish_test/AshishTestApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.ashish_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshishTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshishTestApplication.class, args);
+	}
+
+}
